yungyuc/turgon

commit 637b669b969c8d893f4bb69e069238a70e7147a0

Add a simple weighting function alpha scheme with alpha = 2

spacetime/include/spacetime/Celm.hpp
@@ -40,12 +40,20 @@ template< typename SE >
 inline
 typename CelmBase<SE>::value_type CelmBase<SE>::calc_so1(size_t iv) const
 {
+    // Fetch value.
     const SE se_xn = selm_xn();
     const SE se_xp = selm_xp();
     const value_type upp = se_xn.so0p(iv);
     const value_type upn = se_xp.so0p(iv);
     const SE se_tp = selm_tp();
-    return (upn - upp) / se_tp.dx();
+    const value_type up = se_tp.so0(iv);
+    // alpha-scheme. (alpha = 2)
+    const value_type duxn = (up - upn) / se_xn.dxpos();
+    const value_type duxp = (upp - up) / se_xp.dxneg();
+    const value_type fan = duxn*duxn;
+    const value_type fap = duxp*duxp;
+    constexpr value_type tiny = std::numeric_limits<value_type>::min();
+    return (fap*duxn + fan*duxp) / (fap + fan + tiny);
 }
 
 } /* end namespace spacetime */

spacetime/tests/test_linear_scalar.py
@@ -108,10 +108,10 @@ class LinearScalarSolverTC(unittest.TestCase):
 
         self.svr.march(self.nstep*self.cycle)
         np.testing.assert_allclose(self.svr.get_so0(0), np.sin(self.xcrd),
-                                   rtol=1.e-14, atol=1.e-15)
+                                   rtol=0, atol=1.e-14)
         ones = np.ones(self.svr.grid.nselm, dtype='float64')
         np.testing.assert_allclose(self.svr.get_cfl(), ones,
-                                   rtol=1.e-15, atol=1.e-15)
+                                   rtol=0, atol=1.e-14)
 
     def test_march_fine_interface(self):
 

commit ed82a5e252d5694cad90c1587dd626ec9b5e4519

Make inviscid Burgers equation run seemingly correct
Fix a bug introduced in 637b669 that flipped the sign of ux.

spacetime/include/spacetime/Celm.hpp
@@ -43,13 +43,12 @@ typename CelmBase<SE>::value_type CelmBase<SE>::calc_so1(size_t iv) const
     // Fetch value.
     const SE se_xn = selm_xn();
     const SE se_xp = selm_xp();
-    const value_type upp = se_xn.so0p(iv);
-    const value_type upn = se_xp.so0p(iv);
-    const SE se_tp = selm_tp();
-    const value_type up = se_tp.so0(iv);
+    const value_type upn = se_xn.so0p(iv); // u' at left SE
+    const value_type upp = se_xp.so0p(iv); // u' at right SE
+    const value_type utp = selm_tp().so0(iv); // u at top SE
     // alpha-scheme. (alpha = 2)
-    const value_type duxn = (up - upn) / se_xn.dxpos();
-    const value_type duxp = (upp - up) / se_xp.dxneg();
+    const value_type duxn = (utp - upn) / se_xn.dxpos();
+    const value_type duxp = (upp - utp) / se_xp.dxneg();
     const value_type fan = duxn*duxn;
     const value_type fap = duxp*duxp;
     constexpr value_type tiny = std::numeric_limits<value_type>::min();

spacetime/include/spacetime/SolverBase.hpp
@@ -12,11 +12,32 @@ namespace spacetime
 
 template< typename ST, typename CE, typename SE > inline
 typename SolverBase<ST,CE,SE>::array_type
+SolverBase<ST,CE,SE>::x(bool odd_plane) const
+{
+    const index_type nselm = grid().nselm() - odd_plane;
+    array_type ret(std::vector<size_t>{nselm});
+    for (index_type it=0; it<nselm; ++it) { ret[it] = selm(it, odd_plane).x(); }
+    return ret;
+}
+
+template< typename ST, typename CE, typename SE > inline
+typename SolverBase<ST,CE,SE>::array_type
 SolverBase<ST,CE,SE>::xctr(bool odd_plane) const
 {
     const index_type nselm = grid().nselm() - odd_plane;
     array_type ret(std::vector<size_t>{nselm});
     for (index_type it=0; it<nselm; ++it) { ret[it] = selm(it, odd_plane).xctr(); }
+    return ret;
+}
+
+template< typename ST, typename CE, typename SE > inline
+typename SolverBase<ST,CE,SE>::array_type
+SolverBase<ST,CE,SE>::get_so0p(size_t iv, bool odd_plane) const
+{
+    if (iv >= m_field.nvar()) { throw std::out_of_range("get_so0p(): out of nvar range"); }
+    const index_type nselm = grid().nselm() - odd_plane;
+    array_type ret(std::vector<size_t>{nselm});
+    for (index_type it=0; it<nselm; ++it) { ret[it] = selm(it, odd_plane).so0p(iv); }
     return ret;
 }
 
@@ -106,6 +127,7 @@ void SolverBase<ST,CE,SE>::update_cfl(bool odd_plane)
         selm(ic, odd_plane).update_cfl();
     }
 }
+
 template< typename ST, typename CE, typename SE > inline
 void SolverBase<ST,CE,SE>::march_half_so1(bool odd_plane)
 {
@@ -143,15 +165,18 @@ void SolverBase<ST,CE,SE>::treat_boundary_so1()
 }
 
 template< typename ST, typename CE, typename SE > inline
-void SolverBase<ST,CE,SE>::march_full()
+void SolverBase<ST,CE,SE>::march_half_first()
 {
-    // first half step.
     march_half_so0(false);
     treat_boundary_so0();
     update_cfl(true);
     march_half_so1(false);
     treat_boundary_so1();
-    // second half step.
+}
+
+template< typename ST, typename CE, typename SE > inline
+void SolverBase<ST,CE,SE>::march_half_second()
+{
     march_half_so0(true);
     update_cfl(false);
     march_half_so1(true);

spacetime/include/spacetime/SolverBase_decl.hpp
@@ -62,6 +62,7 @@ public:
     Grid const & grid() const { return m_field.grid(); }
     Grid       & grid()       { return m_field.grid(); }
 
+    array_type x(bool odd_plane) const;
     array_type xctr(bool odd_plane) const;
 
 #define DECL_ST_ARRAY_ACCESS_0D(NAME) \
@@ -81,6 +82,8 @@ public:
 
 #undef DECL_ST_ARRAY_ACCESS_1D
 #undef DECL_ST_ARRAY_ACCESS_0D
+
+    array_type get_so0p(size_t iv, bool odd_plane) const;
 
     size_t nvar() const { return m_field.nvar(); }
 
@@ -108,7 +111,9 @@ public:
     void treat_boundary_so1();
 
     void setup_march() { update_cfl(false); }
-    void march_full();
+    void march_half_first();
+    void march_half_second();
+    void march_full() { march_half_first(); march_half_second(); }
     void march(size_t steps) { for (size_t it=0; it<steps; ++it) { march_full(); } }
 
 protected:

spacetime/include/spacetime/kernel/inviscid_burgers.hpp
@@ -38,6 +38,8 @@ public:
 
     value_type so0p(size_t iv) const;
 
+    value_type & update_cfl();
+
 }; /* end class FelmBase */
 
 /**
@@ -45,9 +47,8 @@ public:
  */
 InviscidBurgersSelm::value_type InviscidBurgersSelm::xn(size_t iv) const
 {
-    const value_type area = x() - xneg();
     const value_type displacement = 0.5 * (x() + xneg()) - xctr();
-    return area * (so0(iv) + displacement * so1(iv));
+    return dxneg() * (so0(iv) + displacement * so1(iv));
 }
 
 /**
@@ -55,9 +56,8 @@ InviscidBurgersSelm::value_type InviscidBurgersSelm::xn(size_t iv) const
  */
 InviscidBurgersSelm::value_type InviscidBurgersSelm::xp(size_t iv) const
 {
-    const value_type area = xpos() - x();
     const value_type displacement = 0.5 * (x() + xpos()) - xctr();
-    return area * (so0(iv) + displacement * so1(iv));
+    return dxpos() * (so0(iv) + displacement * so1(iv));
 }
 
 /**
@@ -96,7 +96,14 @@ InviscidBurgersSelm::value_type InviscidBurgersSelm::so0p(size_t iv) const
     value_type ret = so0(iv);
     ret += (x()-xctr()) * so1(iv); /* displacement in x */
     ret -= hdt() * so1(iv); /* displacement in t */
-    return ret * so0(iv);
+    return ret;
+}
+
+InviscidBurgersSelm::value_type & InviscidBurgersSelm::update_cfl()
+{
+    const value_type hdx = std::min(dxneg(), dxpos());
+    this->cfl() = std::fabs(so0(0)) * field().hdt() / hdx;
+    return this->cfl();
 }
 
 using InviscidBurgersCelm = CelmBase<InviscidBurgersSelm>;

spacetime/include/spacetime/python/common.hpp
@@ -307,6 +307,7 @@ protected:
         (*this)
             .def("__str__", &detail::to_str<wrapped_type>)
             .def_property_readonly("grid", [](wrapped_type & self){ return self.grid().shared_from_this(); })
+            .def("x", &wrapped_type::x, py::arg("odd_plane")=false)
             .def("xctr", &wrapped_type::xctr, py::arg("odd_plane")=false)
             .def_property_readonly("nvar", &wrapped_type::nvar)
             .def_property(
@@ -335,6 +336,7 @@ protected:
                 { return elm_iter_type(self.shared_from_this(), odd_plane, 0, true); }
               , py::arg("odd_plane")=false
             )
+            .def("get_so0p", &wrapped_type::get_so0p, py::arg("iv"), py::arg("odd_plane")=false)
             DECL_ST_WRAP_ARRAY_ACCESS_0D(cfl)
             DECL_ST_WRAP_ARRAY_ACCESS_1D(so0)
             DECL_ST_WRAP_ARRAY_ACCESS_1D(so1)
@@ -344,6 +346,8 @@ protected:
             .def("treat_boundary_so0", &wrapped_type::treat_boundary_so0)
             .def("treat_boundary_so1", &wrapped_type::treat_boundary_so1)
             .def("setup_march", &wrapped_type::setup_march)
+            .def("march_half_first", &wrapped_type::march_half_first)
+            .def("march_half_second", &wrapped_type::march_half_second)
             .def("march_full", &wrapped_type::march_full)
             .def("march", &wrapped_type::march, py::arg("steps"))
         ;

spacetime/tests/test_inviscid_burgers.py
@@ -10,60 +10,46 @@ import libst
 
 class InviscidBurgersSolverTC(unittest.TestCase):
 
+    @staticmethod
+    def _build_solver(resolution):
+
+        # Build grid.
+        xcrd = np.arange(resolution+1) / resolution
+        xcrd *= 2 * np.pi
+        grid = libst.Grid(xcrd)
+        dx = (grid.xmax - grid.xmin) / grid.ncelm
+
+        # Build solver.
+        time_stop = 2*np.pi
+        cfl_max = 1.0
+        dt_max = dx * cfl_max
+        nstep = int(np.ceil(time_stop / dt_max))
+        dt = time_stop / nstep
+        cfl = dt / dx
+        svr = libst.InviscidBurgersSolver(grid=grid, time_increment=dt)
+
+        # Initialize.
+        svr.set_so0(0, np.sin(xcrd))
+        svr.set_so1(0, np.cos(xcrd))
+        svr.setup_march()
+
+        return nstep, xcrd, svr
+
     def setUp(self):
 
-        self.dt = 0.1
-        self.grid10 = libst.Grid(0, 10, 10)
-        self.sol10 = libst.InviscidBurgersSolver(
-            grid=self.grid10, time_increment=self.dt)
-
-        self.sol10.so0.fill(1)
-        self.sol10.so1.fill(1)
-        for elm in self.sol10.selms(odd_plane=False):
-            r = elm.grid.xmin + elm.xctr / (elm.grid.xmax-elm.grid.xmin)
-            v = np.sin(r * np.pi * 2)
-            dv = np.cos(r * np.pi * 2) * 2 * np.pi
-            elm.set_so0(0, 3) # FIXME: find a generic way to initialize.
-            elm.set_so1(0, 0)
+        self.resolution = 8
+        self.nstep, self.xcrd, self.svr = self._build_solver(self.resolution)
+        self.cycle = 2
 
     def test_nvar(self):
 
-        self.assertEqual(1, self.sol10.nvar)
+        self.assertEqual(1, self.svr.nvar)
 
-    def test_march_half_so0(self):
+    def test_result_bound(self):
 
-        self.sol10.march_half_so0(odd_plane=False)
-        res = [s.get_so0(0) for s in self.sol10.selms(odd_plane=True)]
-        self.assertEqual(10, len(res))
-        self.assertEqual([3]*10, res)
-
-    def test_march_half_so1(self):
-
-        self.sol10.march_half_so1(odd_plane=False)
-        res = [s.get_so1(0) for s in self.sol10.selms(odd_plane=True)]
-        self.assertEqual(10, len(res))
-        self.assertEqual([0]*10, res)
-
-
-class InviscidBurgersSelm(unittest.TestCase):
-
-    def setUp(self):
-        self.dt = 0.2
-        self.grid10 = libst.Grid(0, 10, 10)
-        self.sol10 = libst.InviscidBurgersSolver(
-            grid=self.grid10, time_increment=self.dt)
-        self.se0 = self.sol10.selm(0)
-        self.se9 = self.sol10.selm(9)
-        self.se10 = self.sol10.selm(10)
-        # Set trivial solution.
-        self.sol10.so0.fill(1)
-        self.sol10.so1.fill(0)
-
-    def test_flux(self):
-
-        self.assertEqual(0.5, self.se0.xn(0))
-        self.assertEqual(0.5, self.se0.xp(0))
-        self.assertEqual(0.05, self.se0.tn(0))
-        self.assertEqual(0.05, self.se0.tp(0))
+        for it in range(self.nstep*self.cycle):
+            res = self.svr.get_so0(0)
+            self.assertLessEqual(res.max(), 1)
+            self.assertGreaterEqual(res.min(), -1)
 
 # vim: set et sw=4 ts=4:

spacetime/tests/test_linear_scalar.py
@@ -26,10 +26,7 @@ class LinearScalarSolverTC(unittest.TestCase):
         nstep = int(np.ceil(time_stop / dt_max))
         dt = time_stop / nstep
         cfl = dt / dx
-        svr = libst.LinearScalarSolver(
-            grid=grid,
-            time_increment=dt,
-        )
+        svr = libst.LinearScalarSolver(grid=grid, time_increment=dt)
 
         # Initialize.
         svr.set_so0(0, np.sin(xcrd))