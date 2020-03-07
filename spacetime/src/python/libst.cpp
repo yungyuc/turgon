a33845ffc2c6adf13a8282087d35ebfd1a97e44e
Yung-Yu Chen <yyc@solvcon.net>
2020-03-07 23:27:32 +0800

    Remove unnecessary callbacks used for wrapping

--- spacetime/src/python/libst.cpp
+++ spacetime/src/python/libst.cpp
@@ -17,33 +17,62 @@
 namespace
 {
 
-PyObject * initialize_spacetime(pybind11::module * mod)
+template< typename WST, typename WCET, typename WSET >
+void add_solver(pybind11::module & mod, const std::string & name, const std::string & desc)
 {
-    namespace spy = spacetime::python;
-    namespace mpy = modmesh::python;
-    import_array1(nullptr); // or numpy c api segfault.
-    mod->doc() = "_libst: One-dimensional space-time CESE method code";
-    mpy::WrapConcreteBuffer::commit(*mod, "ConcreteBuffer", "ConcreteBuffer");
-    spy::WrapGrid::commit(mod, "Grid", "Spatial grid data");
-    spy::WrapField::commit(mod, "Field", "Solution data");
-    return mod->ptr();
+    WST::commit
+    (
+        &mod
+      , (name + "Solver").c_str()
+      , ("Solving algorithm of " + desc).c_str()
+    );
+    WCET::commit
+    (
+        &mod
+      , (name + "Celm").c_str()
+      , ("Conservation element of " + desc).c_str()
+    );
+    WSET::commit
+    (
+        &mod
+      , (name + "Selm").c_str()
+      , ("Solution element of " + desc).c_str()
+    );
 }
 
 } /* end namespace */
 
 PYBIND11_MODULE(_libst, mod) // NOLINT
 {
+    namespace mpy = modmesh::python;
     namespace spy = spacetime::python;
-    spy::ModuleInitializer::get_instance()
-        .add(initialize_spacetime)
-        .add_solver<spy::WrapSolver, spy::WrapCelm, spy::WrapSelm>
-        (&mod, "", "no equation")
-        .add_solver<spy::WrapLinearScalarSolver, spy::WrapLinearScalarCelm, spy::WrapLinearScalarSelm>
-        (&mod, "LinearScalar", "a linear scalar equation")
-        .add_solver<spy::WrapInviscidBurgersSolver, spy::WrapInviscidBurgersCelm, spy::WrapInviscidBurgersSelm>
-        (&mod, "InviscidBurgers", "the inviscid Burgers equation")
-        .initialize(&mod)
-    ;
+
+    mod.doc() = "_libst: One-dimensional space-time CESE method code";
+
+    mpy::initialize(mod); // numpy is imported here as early as possible or it crashes.
+
+    spy::WrapGrid::commit(&mod, "Grid", "Spatial grid data");
+    spy::WrapField::commit(&mod, "Field", "Solution data");
+
+    add_solver
+    <
+        spy::WrapSolver
+      , spy::WrapCelm
+      , spy::WrapSelm
+    >(mod, "", "no equation");
+
+    add_solver
+    <
+        spy::WrapLinearScalarSolver
+      , spy::WrapLinearScalarCelm
+      , spy::WrapLinearScalarSelm
+    >(mod, "LinearScalar", "a linear scalar equation");
+
+    add_solver<
+        spy::WrapInviscidBurgersSolver
+      , spy::WrapInviscidBurgersCelm
+      , spy::WrapInviscidBurgersSelm
+    >(mod, "InviscidBurgers", "the inviscid Burgers equation");
 }
 
 // vim: set et sw=4 ts=4:
