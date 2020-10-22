38a4d66784497f65032bea2a63011f70f5f375ba
j8xixo12 <as2266317@gmail.com>
2020-10-22 21:00:17 +0800

    fix: revert test_linear_scalar.py and fix coding style

--- spacetime/tests/test_linear_scalar.py
+++ spacetime/tests/test_linear_scalar.py
@@ -237,4 +237,102 @@ class NormTC(unittest.TestCase):
             )  # FIX ME: I don't know how to determine assert condition
 
 
+class LinearScalarErrorTC(unittest.TestCase):
+    """
+    Compare linear solver's solution wtih exact solution, because solutions of computation field is a vector,
+    therefore compared the norm of difference bewteen solver's solution and exact solution.
+
+    By comparing norm of difference bewteen solver's solution and exact solution we can check if the linear solver is work properly
+    or check if the solver's mathematical model is correct.
+    """
+
+    @staticmethod
+    def _build_solver(resolution):
+        """ Create linear solver object for norm test
+            Args:
+              resolution:
+                Grid number
+            Return:
+                A well set linear solver object
+        """
+        grid = libst.Grid(0, 4 * 2 * np.pi, resolution)
+        cfl = 1
+        dx = (grid.xmax - grid.xmin) / grid.ncelm
+        dt = dx * cfl
+        svr = libst.LinearScalarSolver(grid=grid, time_increment=dt)
+
+        # Initialize
+        for e in svr.selms(odd_plane=False):
+            if e.xctr < 2 * np.pi or e.xctr > 4 * np.pi:
+                v = 0
+                dv = 0
+            else:
+                v = np.sin(e.xctr)
+                dv = np.cos(e.xctr)
+            e.set_so0(0, v)
+            e.set_so1(0, dv)
+
+        svr.setup_march()
+
+        return svr
+
+    @staticmethod
+    def _exact_solution(svr, iter_num, gfun):
+        """ Calculate exact solution of the 1D advection equation
+            Args:
+              svr:
+                Linear wave solver
+              iter_num:
+                How many step will be executed
+              gfun:
+                System function
+             Return:
+                A ndarray which content current time step solution
+        """
+        v = np.array([])
+        for e in svr.selms(odd_plane=False):
+            x = (
+                e.xctr - iter_num * svr.dt
+            ) % svr.grid.xmax  # dealt with boundary treatment
+            if x < 2 * np.pi or x > 4 * np.pi:
+                v = np.append(v, 0)
+            else:
+                v = np.append(v, gfun(x))
+        return v
+
+    @staticmethod
+    def _norm(vec, ord=1):
+        """ Calculate the vector norm
+            Args:
+              vec:
+                Vector for norm calculation
+              ord:
+                Norm order
+            Return:
+                A double number, vector norm
+        """
+        res = 0
+        for ele in vec:
+            res += abs(ele) ** ord
+        return res ** (1 / ord)
+
+    def setUp(self):
+
+        self.resolution = 256
+        self.svr = LinearScalarErrorTC._build_solver(self.resolution)
+        self.cycle = range(1001)
+
+    def test_norm_error(self):
+
+        for i in self.cycle:
+            self.svr.march_alpha2(1)
+            error_norm = LinearScalarErrorTC._norm(
+                LinearScalarErrorTC._exact_solution(self.svr, i + 1, np.sin)
+                - self.svr.get_so0(0).ndarray,
+                1,
+            )
+            self.assertLessEqual(
+                error_norm, 1e-11
+            )  # FIX ME: I don't know how to determine assert condition
+
 # vim: set et sw=4 ts=4:
